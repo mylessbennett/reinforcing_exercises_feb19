60c252e9f60b3fec108e6db3ab4f396e2c21f2cc
Myles Bennett <myles.bennett@hotmail.com>
2019-02-19 13:51:59 -0500

    completed exercise 3

--- exercise.py
+++ exercise.py
@@ -9,15 +9,25 @@ class Person:
     def __str__(self):
         return
 
+    def feeling(self):
+        for i in emotions:
+            if emotions[i] == 3:
+                print("I'm feeling a high amount of {}.".format(i))
+            elif emotions[i] == 2:
+                print("I'm feeling a medium amount of {}.".format(i))
+            elif emotions[i] == 1:
+                print("I'm feeling a low amount of {}.".format(i))
+
 
 
 #---------------------------------------------------------------
 # Exercise 1
-myles_emotions = {'happiness': 3, 'fear': 1, 'sadness': 2}
+emotions = {'happiness': 3, 'fear': 1, 'sadness': 2}
 
-myles = Person('Myles', myles_emotions)
+myles = Person('Myles', emotions)
 #---------------------------------------------------------------
 # Exercise 3
+myles.feeling()
 
 
 #---------------------------------------------------------------

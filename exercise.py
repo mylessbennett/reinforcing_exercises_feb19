dd56ce61432d3c9d7e6b6479b7f00f41fb3c6d0d
Myles Bennett <myles.bennett@hotmail.com>
2019-02-19 13:43:40 -0500

    completed exercise 2

--- exercise.py
+++ exercise.py
@@ -1,9 +1,21 @@
-# Exercise 1
-emotions = {'happiness': 3, 'fear': 1, 'sadness': 2}
-#---------------------------------------------------------------
 # Exercise 2
+class Person:
+    """ Discribes how a person is feeling """
+
+    def __init__(self, name, emotions):
+        self.name = name
+        self.emotions = emotions
+
+    def __str__(self):
+        return
 
 
+
+#---------------------------------------------------------------
+# Exercise 1
+myles_emotions = {'happiness': 3, 'fear': 1, 'sadness': 2}
+
+myles = Person('Myles', myles_emotions)
 #---------------------------------------------------------------
 # Exercise 3
 
